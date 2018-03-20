d856e9f6c548fa9eefeba321bacfd5c85e1136fa
Ali Berlin Johnson <berlinjohnson@gmail.com>
2018-03-20 14:08:30 -0700

    tab sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,40 @@ $(function() {
   console.log('Hi');
 
   console.log(projects);
+
+  // NAV BAR
+  var showSection = function(path) {
+    // Switch tab highlighed
+    $('nav button').removeClass('is-selected').blur();
+    $('nav button[data-path="' + path + '"]').addClass('is-selected');
+
+    // Switch section shown
+    $('section').addClass('is-hidden');
+    $('section[data-path="' + path + '"]').removeClass('is-hidden');
+  };
+
+  var navigateTo = function(e) {
+    e.preventDefault;
+
+    var path = $(this).data('path');
+    if (location.pathname === path) {
+      return;
+    };
+
+    history.pushState({}, '', path);
+    showSection(path);
+  };
+
+  // Browser's back or forward action
+  window.onpopstate = function(e) {
+    showSection(location.pathname);
+  };
+
+  // First page load
+  showSection(location.pathname);
+
+  // TAB CLICKS
+  $('#aboutNav').on('click', navigateTo);
+  $('#portfolioNav').on('click', navigateTo);
+  $('#resumeNav').on('click', navigateTo);
 });
